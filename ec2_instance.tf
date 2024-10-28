d2d2506098782dea149d90ae8b5113b2ac48e41c
Vladyslav Tuvaiev <vlad.tuvaev@gmail.com>
2024-10-28 13:00:16 +0100

    ec2 add

---
+++ ec2_instance.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_vm" {
+  ami           = "ami-0c55b159cbfafe1f0" # Используй нужный AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "My test VM"
+  }
+}
