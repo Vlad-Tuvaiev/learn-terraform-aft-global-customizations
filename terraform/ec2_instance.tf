8fb89f517bd37e9f9bd4c0b9c4478ff9a15b1b89
Vladyslav Tuvaiev <vlad.tuvaev@gmail.com>
2024-10-28 14:37:30 +0100

    fix

--- terraform/ec2_instance.tf
+++ terraform/ec2_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_vm" {
-  ami           = "ami-0c55b159cbfafe1f0" # Используй нужный AMI
+  ami           = "ami-02db68a01488594c5" # Используй нужный AMI
   instance_type = "t2.micro"
 
   tags = {
--- terraform/s3-public-blob.tf
+++ terraform/s3-public-blob.tf
@@ -1,14 +1,9 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-test-bucket-${random_string.suffix.result}"
+  bucket = "my-test-bucket-qqwwwssszzzxxx"
   acl    = "private"
 
   tags = {
     Name        = "My test bucket"
     Environment = "Test"
   }
-}
-
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
 }
